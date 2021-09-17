d5f439e3b9fd0866e43910f6288290cde5a61da6
Matthew Guptil <matthew.d.guptil@exxonmobil.com>
2021-09-17 16:09:42 -0500

    Ranking of Grade Attempt 1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,54 @@ namespace GradeBook.GradeBooks
         }
 
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int totStu = Students.Count;
+
+            if (totStu < 5) {
+                throw new InvalidOperationException();
+            }
+
+            List<double> classTotal = new List<double>();
+
+            foreach (Student stu in Students) {
+                classTotal.Add(stu.AverageGrade);
+            }
+            classTotal.Sort();
+
+            //double per20 = Math.Round(totStu * 0.2, 0);
+            int per20 = totStu / 5;
+            int stuNum = 0;
+
+            foreach (double currGrade in classTotal)
+            {
+                if (currGrade > averageGrade)
+                {
+                    break;
+                }
+                stuNum = stuNum + 1;
+            }
+
+            if (stuNum >= (totStu-per20))
+            {
+                return 'A';
+            }
+            else if ((totStu-per20) > stuNum && stuNum >= (totStu-per20*2))
+            {
+                return 'B';
+            }
+            else if ((totStu - 2*per20) > stuNum && stuNum >= (totStu - per20 * 3))
+            {
+                return 'C';
+            }
+            else if ((totStu - 3*per20) > stuNum && stuNum >= (totStu - per20 * 4))
+            {
+                return 'D';
+            }
+
+
+
+            return 'F';
+        }
     }
 }
